91529137e42ab04f06ed315d6e31e536ba6a4195
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-14 11:53:38 +0200

    Add Calculator.js boilerplate JSX

---
+++ src/components/Calculator.js
@@ -0,0 +1,16 @@
+const Calculator = () => {
+  return (
+    <main className="calculator">
+      <header className="calc-canvas"></header>
+
+      <div className="calc-grid">
+        <section className="calc-numbers">
+          <buttton id="id" className="btn"></buttton>
+        </section>
+        <section className="calc-operators"></section>
+      </div>
+    </main>
+  );
+};
+
+export default Calculator;
