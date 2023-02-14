8153f472f72e55fb40e6c011c3a8d72d26acd2c6
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-14 17:01:35 +0200

    Add calc output styling

---
+++ src/components/Calculator.js
@@ -0,0 +1,21 @@
+import '../calculator.css';
+
+const Calculator = () => (
+  <main className="calculator">
+    <header className="calc-output">0</header>
+
+    <div className="calc-grid">
+      <section className="calc-numbers">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, corrupti. Sequi, eveniet
+        placeat, dolor sed, animi non fugit soluta quis illum quia nulla ipsum dolores! Voluptates
+        quia rem, aperiam quibusdam quasi error sit? Eos animi cum deserunt error veritatis
+        dignissimos blanditiis accusantium expedita dolores similique tempore iure iusto, officia
+        et.
+      </section>
+
+      <section className="calc-operators">Lorem ipsum dolor sit amet.</section>
+    </div>
+  </main>
+);
+
+export default Calculator;
